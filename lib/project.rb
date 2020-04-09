a5760ebbb7ed5fe55b3f31da4fe0fa99d05b04dc
Kevin Mathias <komathias1@gmail.com>
2020-04-09 00:31:17 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,10 +1,10 @@
 
 
 class Projects
-  attr_reader :name, :backers
+  attr_reader :title, :backers
   
-  def initialize(name)
-    @name = name
+  def initialize(title)
+    @title = title
     @backers = []
   end
   
