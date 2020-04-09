a3f841a488fcd71eb2e0b6f9249152abf5451831
Kevin Mathias <komathias1@gmail.com>
2020-04-09 01:10:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -10,7 +10,7 @@ class Project
   
   def add_backer(backer)
     @backers << backer
-   backer.back_project(self) unless backer.backed_projects.include?(self)
+   backer.back_project(self) if backer.backed_projects.include?(self) == false
   end
   
   
